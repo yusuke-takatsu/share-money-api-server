87bad8695249b7ede87802d9cc9d8167f970bedb
github VinousTy <github ys.taka44@gmail.com>
2024-04-29 20:22:12 +0900

    feat:add auth error exception

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Middleware;
 
+use App\Exceptions\User\Auth\AuthErrorException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+    /**
+     * {@inheritDoc}
+     *
+     * @param [type] $request
+     * @param array $guards
+     * @return void
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        if ($guards[0] === config('auth.guards.user')) {
+            throw new AuthErrorException();
+        }
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
